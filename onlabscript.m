1769766ea694ae4e1370585f2d09b4fe835eaa70
patakibalazs03 <pataki.balazs03@gmail.com>
2025-05-09 14:22:08 +0200

    Add files via upload
    2025.05.09

---
+++ onlabscript.m
@@ -0,0 +1,20 @@
+clear;
+%% Parameters
+La=3; %H, inductance of the armature winding
+Ra=6; %Ohm, resistance of the armature winding
+K_b=3; %V/rad/s the back emf constant 
+
+K_t=2; %Nm/Amp the torque constant
+
+J = 2; % Moment inertia of robot KG*m^2
+M = 5; % Robot mass in KG
+c = 0; % Location of center of gravity of robot. x=d y=o in robot frame
+R = 0.02;%radius of wheel in meter
+L = 0.1;%distance between wheel in meter
+
+%% Real Motor Parameters
+La=0.94; %H, inductance of the armature winding
+Ra=1; %Ohm, resistance of the armature winding
+K_b=0.0301; %V/rad/s the back emf constant 
+
+K_t=0.0300; %Nm/Amp the torque constant
---
+++ startup.m
@@ -0,0 +1,8 @@
+% Add custom paths
+addpath(genpath('C:\Users\Asus\Documents\MATLAB\MRSTB'));
+
+% Run a specific script at startup
+run('C:\Users\Asus\Documents\MATLAB\MRSTB\startMobileRoboticsSimulationToolbox.m');
+
+% Display a message
+disp('MATLAB startup complete. Your script has been executed. :)');
