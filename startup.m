d2ae02f8b33c3d544cf66c2b5a72af441300425f
patakibalazs03 <pataki.balazs03@gmail.com>
2025-05-09 14:31:37 +0200

    Update startup.m
    Adjusted teh startup code

--- startup.m
+++ startup.m
@@ -1,8 +1,8 @@
 % Add custom paths
-addpath(genpath('C:\Users\Asus\Documents\MATLAB\MRSTB'));
+addpath(genpath('C:\Users\Asus\Documents\GitHub\Onlab2025\MRSTB'));
 
 % Run a specific script at startup
-run('C:\Users\Asus\Documents\MATLAB\MRSTB\startMobileRoboticsSimulationToolbox.m');
+run('C:\Users\Asus\Documents\GitHub\Onlab2025\MRSTB\startMobileRoboticsSimulationToolbox.m');
 
 % Display a message
 disp('MATLAB startup complete. Your script has been executed. :)');
