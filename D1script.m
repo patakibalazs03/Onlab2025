8e3a693b9577ca81fe5be30b1aa75c66111973e4
patakibalazs03 <pataki.balazs03@gmail.com>
2025-05-17 21:02:16 +0200

    05.17

---
+++ D1script.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+%% Parameters
+La=1.5e-3; %H, inductance of the armature winding
+Ra=1.2; %Ohm, resistance of the armature winding
+K_b=0.02; %V/rad/s the back emf constant 
+K_t=0.02; %Nm/A the torque constant
+
+J = 3e-4; % Moment inertia of wheel kg*m^2
+M = 8; % Robot mass in KG
+c = 0; % Location of center of gravity of robot. x=d y=o in robot frame
+R = 0.05; %radius of wheel in meter
+L = 0.15; %distance between wheel in meter
+b = 5e-4; %Damping coefficient per wheel Nm*s/rad
+
+C = M*R^2+J; %constant for the wheel
