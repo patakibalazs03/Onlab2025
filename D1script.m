b20261dcb2920601be22a700a8778167d1f960d2
patakibalazs03 <pataki.balazs03@gmail.com>
2025-05-27 20:56:27 +0200

    Work

--- D1script.m
+++ D1script.m
@@ -4,7 +4,7 @@ clear;
 La=1.5e-3; %H, inductance of the armature winding
 Ra=1.2; %Ohm, resistance of the armature winding
 K_b=0.02; %V/rad/s the back emf constant 
-K_t=0.02; %Nm/A the torque constant
+K_i=0.02; %Nm/A the torque constant
 
 J = 3e-4; % Moment inertia of wheel kg*m^2
 M = 8; % Robot mass in KG
